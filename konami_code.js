ec2ad115d82465b7c2ada37bb69b32dce6828576
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-08 04:25:15 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,19 @@ const codes = [
 
 function init() {
   document.body.addEventListener('keydown', event => {
-    console.log(event.key)
+    var keys = [];
+    //keys.push(event)
+
+    for (let i = keys.length - 1; i < codes.length){
+      if (i === -1 && event === codes[0]){
+        keys.push(event)    
+      }
+      else if (i >= 0 && event === codes[i + 1]){
+        keys.push(event)
+      }
+      return keys = []
+      
+    }
   },{
     capture: false
   })
