0448f4d82ac6955ea714800a50355bd91adaa4b5
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-08 04:41:57 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,28 +13,20 @@ const codes = [
 
 function init() {
   document.body.addEventListener('keydown', event => {
-    var keys = [];
-    //keys.push(event)
+    var correctSeq = [];
+    correctSeq.push(event)
 
-    for (let i = keys.length - 1; i < codes.length;){
-      if (i === -1 && event === codes[0]){
-        console.log('1')
-        keys.push(event)
+    for (let i in correctSeq){
+      if (correctSeq[i] === codes[i]){
+        continue;
       }
-      else if (i >= 0 && event === codes[i + 1]){
-        console.log('2')
-        keys.push(event)
-      }
-      else {
-        console.log('3')
-        keys = []
-      }
-console.log('4')
-      if (keys.length === codes.length){
-        console.log(keys)
-      }
+      else correctSeq = []
     }
-  },{
+
+    if (correctSeq.length === codes.length){
+      alert('Congratulations!!')
+    }
+    },{
     capture: false
   })
 }
