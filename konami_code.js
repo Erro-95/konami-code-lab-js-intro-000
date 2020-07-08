0ecbd04420b3d64173542fdbb7c968f7db9e3b9c
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-08 05:05:22 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -22,7 +22,7 @@ function init() {
       }
     }
 
-console.console.log(correctSeq);
+console.log(correctSeq);
     if (correctSeq.length === codes.length){
       alert('Congratulations!!')
       correctSeq = []
